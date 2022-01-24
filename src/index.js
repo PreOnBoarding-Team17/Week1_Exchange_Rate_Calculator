54f72249e9e461c25315bdcbbbd55017f4da2e92
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-24 22:41:31 +0900

    Add: CRA 생성

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
