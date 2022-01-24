f80f6c54fb78710268ace70604480f117f79990c
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-25 01:34:57 +0900

    Fix: 환경설정 수정

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import App from "App";
-import React from "react";
-import ReactDOM from "react-dom";
-import reportWebVitals from "reportWebVitals";
+import App from 'App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 reportWebVitals();
