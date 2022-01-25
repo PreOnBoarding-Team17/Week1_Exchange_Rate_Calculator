0f1921ee01c45314151975d37fd924a4e37ec966
som-syom <dhyeon@student.42seoul.kr>
2022-01-25 18:24:47 +0900

    Feat : input관리, useEffect 수정

--- src/Components/Second/index.jsx
+++ src/Components/Second/index.jsx
@@ -1,33 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import TabTitle from 'Components/Second/TabTitle';
 import TabContent from 'Components/Second/TabContent';
-import Get_API from 'Utils/Api';
 import 'Components/Second/scss/Second.scss';
 
 export default function Second() {
   const currencies = ['USD', 'CAD', 'KRW', 'HKD', 'JPY', 'CNY'];
 
-  const [number, setNumber] = useState(1000);
+  const [number, setNumber] = useState('1,000');
   const [currency, setCurrency] = useState('USD'); // select 선택
-  const [money, setMoney] = useState('CAD'); // content 선탹
-
-  useEffect(async () => {
-    console.log(await Get_API());
-  }, []);
+  const [exchangeTo, setExchangeTo] = useState('CAD'); // content 선택
 
   const onChangeInput = ({ target: { value } }) => {
-    if (!isNaN(value[value.length - 1]) || value[value.length - 1] === '.') {
-      // 콤마 추가 필요
-      if (parseInt(value) >= 1000) {
-        setNumber(1000);
-      } else {
+    if (value.length === 0) setNumber('');
+    else if (
+      value[value.length - 1] === '.' ||
+      !isNaN(value[value.length - 1])
+    ) {
+      if (value.replace(/,/g, '') >= 1000) {
+        setNumber(parseFloat(1000).toLocaleString('en'));
+      } else if (value[value.length - 1] === '.') {
         setNumber(value);
+      } else {
+        setNumber(Number(value.replace(/,/g, '')));
       }
     }
   };
 
   const onChangeSelect = ({ target: { value } }) => {
     setCurrency(value);
+    if (value === 'USD') setExchangeTo('CAD');
+    else setExchangeTo('USD');
   };
 
   return (
@@ -36,10 +38,9 @@ export default function Second() {
         <div className="Calc-Container__Top">
           <input
             type="text"
-            value={number}
+            value={number || ''}
             onChange={onChangeInput}
             className="Calc-Container__Top__input"
-            onChange={onChangeInput}
           />
           <select
             name="Country"
@@ -60,11 +61,20 @@ export default function Second() {
             {currencies.map(
               (el, idx) =>
                 currency !== el && (
-                  <TabTitle name={el} key={idx} setMoney={setMoney} />
+                  <TabTitle
+                    name={el}
+                    key={idx}
+                    exchangeTo={exchangeTo}
+                    setExchangeTo={setExchangeTo}
+                  />
                 ),
             )}
           </ul>
-          <TabContent number={number} money={money} />
+          <TabContent
+            number={number.length !== 0 ? number : 0}
+            currency={currency}
+            exchangeTo={exchangeTo}
+          />
         </div>
       </div>
     </section>
