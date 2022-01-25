0305152d8a89f4608fad5f36df6bb2fa12e05bca
developjik <developjik@gmail.com>
2022-01-25 23:06:32 +0900

    Refactor API 함수 수정

--- src/API/index.jsx
+++ src/API/index.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const BASE_URL = `http://www.apilayer.net/api/live?access_key=${process.env.REACT_APP_API_KEY}`;
+// const BASE_URL = `http://www.apilayer.net/api/live?access_key=${process.env.REACT_APP_API_KEY}`;
+const BASE_URL = `http://www.apilayer.net/api/live?access_key=804703a74bf6675901d26c72b6db8091`;
 
-function Get_API() {
-  return axios.get(BASE_URL);
+function Get_API(params) {
+  return axios.get(BASE_URL, { params });
 }
 
 export const API = {
