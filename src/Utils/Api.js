593548e4d1160f8f4ace5fe55dc863127e143f5e
som-syom <dhyeon@student.42seoul.kr>
2022-01-25 18:27:23 +0900

    Add : api 함수 추가

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -1,14 +1,3 @@
-import axios from 'axios';
-
 const BASE_URL = `http://www.apilayer.net/api/live?access_key=${process.env.REACT_APP_API_KEY}`;
 
-const Get_API = async () => {
-  return await axios.get(BASE_URL).then((res) => {
-    return {
-      timestamp: res.data.timestamp,
-      quotes: res.data.quotes,
-    };
-  });
-};
-
-export default Get_API;
+export default BASE_URL;
