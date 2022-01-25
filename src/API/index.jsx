ab434b6e753dfceb3d2913d2ec5c080574d53ccc
developjik <developjik@gmail.com>
2022-01-25 23:13:58 +0900

    Chore 주석해제

--- src/API/index.jsx
+++ src/API/index.jsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-// const BASE_URL = `http://www.apilayer.net/api/live?access_key=${process.env.REACT_APP_API_KEY}`;
-const BASE_URL = `http://www.apilayer.net/api/live?access_key=804703a74bf6675901d26c72b6db8091`;
+const BASE_URL = `http://www.apilayer.net/api/live?access_key=${process.env.REACT_APP_API_KEY}`;
 
 function Get_API(params) {
   return axios.get(BASE_URL, { params });
