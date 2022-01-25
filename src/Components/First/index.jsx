149873038654e776e587bdf1eda9ef2a65d82865
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-01-25 20:50:04 +0900

    Fix: HTML 구성 수정, 함수 소수점 이슈 해결, input 입력값 에러 해결

--- src/Components/First/index.jsx
+++ src/Components/First/index.jsx
@@ -5,10 +5,11 @@ import React, { useEffect, useRef, useState } from 'react';
 export default function First() {
   const inputRef = useRef(null);
   const [data, setData] = useState([]);
-  const [select, setSelect] = useState('USDKRW');
-  const [exchange, setExchange] = useState(0);
   const [value, setValue] = useState(0);
+  const [exchange, setExchange] = useState(0);
+  const [select, setSelect] = useState('USDKRW');
 
+  // API 불러오기
   useEffect(() => {
     async function getAPI() {
       await API.Get_API({
@@ -20,14 +21,10 @@ export default function First() {
     getAPI();
   }, []);
 
-  // const onChangeSelect = (e) => {
-  //   const key = Object.keys(data);
-  //   const answer = key.find((element) => element.includes(select));
-  //   setExchange(answer && data[answer].toFixed(2));
-  // };
-
+  // 환율 변수
   const nowCurrency = Number(data[select]).toFixed(2);
 
+  // 송금액 계산 함수
   const handleSubmit = (e) => {
     e.preventDefault();
     let { value } = inputRef.current;
@@ -43,37 +40,47 @@ export default function First() {
       return;
     }
     setValue(numberValue);
-    setExchange(Number(numberValue * nowCurrency).toLocaleString());
+    setExchange(
+      Number(numberValue * nowCurrency).toLocaleString(undefined, {
+        minimumFractionDigits: 2,
+      }),
+    );
   };
-
-  // useEffect(() => {
-  //   onChangeSelect();
-  // }, [onChangeSelect]);
 
   return (
     <section className="First__Container">
-      <h1>환율 계산</h1>
-      <form className="FormStyle" onSubmit={handleSubmit}>
-        <p className="SendCountry">송금국가 : 미국(USD)</p>
-        <label htmlFor="country">수취국가 : </label>
-        <select
-          id="country"
-          onChange={(event) => setSelect(event.target.value)}
-          className="ReceiveCountry">
-          <option value="USDKRW">한국(KRW)</option>
-          <option value="USDJPY">일본(JPY)</option>
-          <option value="USDPHP">필리핀(PHP)</option>
-        </select>
-        <p className="ExchageRate">
-          환율 : <span>{`${nowCurrency} ${select.slice(3)}`}</span>
-          /USD
-        </p>
-        <label>송금액 : </label>
-        <input ref={inputRef} />
-        <button type="submit">submit</button>
+      <h1 className="First__Title">환율 계산</h1>
+      <form className="First__Form" onSubmit={handleSubmit}>
+        <div className="First__Box">
+          <h3>송금국가</h3>
+          <p>미국(USD)</p>
+        </div>
+        <div className="First__Box">
+          <h3>수취국가</h3>
+          <select onChange={(event) => setSelect(event.target.value)}>
+            <option value="USDKRW">한국(KRW)</option>
+            <option value="USDJPY">일본(JPY)</option>
+            <option value="USDPHP">필리핀(PHP)</option>
+          </select>
+        </div>
+        <div className="First__Box">
+          <h3>환율</h3>
+          <p className="First__Exchange_Rate">
+            <span>{`${nowCurrency} ${select.slice(3)}`}</span>
+            /USD
+          </p>
+        </div>
+        <div className="First__Box LastBox">
+          <h3>송금액</h3>
+          <input className="First__Input" type="number" ref={inputRef} />
+          <p> USD</p>
+        </div>
+        <button className="First__Submit" type="submit">
+          submit
+        </button>
       </form>
-      <div>
-        {value > 0 && `수취금액은 ${exchange} ${select.slice(3)}입니다`}
+      <div className="First__Result">
+        {value > 0 && `수취금액은 ${exchange} ${select.slice(3)} 입니다.`}
       </div>
     </section>
   );
