efa3bfd2a9f5905e0e68cba842dec58718c84e78
BobaeKeum <aann0221@gmail.com>
2022-01-25 19:40:10 +0900

    feat: 계산기1 기능 구현

--- src/Components/First/index.jsx
+++ src/Components/First/index.jsx
@@ -7,6 +7,8 @@ export default function First() {
   const [data, setData] = useState([]);
   const [select, setSelect] = useState('USDKRW');
   const [exchange, setExchange] = useState(0);
+  const [value, setValue] = useState(0);
+
   useEffect(() => {
     async function getAPI() {
       await API.Get_API({
@@ -17,20 +19,41 @@ export default function First() {
     }
     getAPI();
   }, []);
-  const onChangeSelect = () => {
-    const key = Object.keys(data);
-    const answer = key.find((element) => element.includes(select));
-    setExchange(answer && data[answer].toFixed(2));
+
+  // const onChangeSelect = (e) => {
+  //   const key = Object.keys(data);
+  //   const answer = key.find((element) => element.includes(select));
+  //   setExchange(answer && data[answer].toFixed(2));
+  // };
+
+  const nowCurrency = Number(data[select]).toFixed(2);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let { value } = inputRef.current;
+    const numberValue = Number(value);
+    if (numberValue < 0 || numberValue > 10000) {
+      alert('송금액이 바르지 않습니다.');
+      inputRef.current.value = '';
+      return;
+    }
+    if (typeof numberValue !== 'number') {
+      alert('송금액이 바르지 않습니다.');
+      inputRef.current.value = '';
+      return;
+    }
+    setValue(numberValue);
+    setExchange(Number(numberValue * nowCurrency).toLocaleString());
   };
-  useEffect(() => {
-    onChangeSelect();
-  }, [onChangeSelect]);
-  console.log(exchange);
+
+  // useEffect(() => {
+  //   onChangeSelect();
+  // }, [onChangeSelect]);
 
   return (
     <section className="First__Container">
       <h1>환율 계산</h1>
-      <form className="FormStyle">
+      <form className="FormStyle" onSubmit={handleSubmit}>
         <p className="SendCountry">송금국가 : 미국(USD)</p>
         <label htmlFor="country">수취국가 : </label>
         <select
@@ -42,14 +65,16 @@ export default function First() {
           <option value="USDPHP">필리핀(PHP)</option>
         </select>
         <p className="ExchageRate">
-          환율 : <span>1달러로 계산된 값이 들어올 곳</span>
+          환율 : <span>{`${nowCurrency} ${select.slice(3)}`}</span>
           /USD
         </p>
         <label>송금액 : </label>
         <input ref={inputRef} />
-        <button>submit</button>
+        <button type="submit">submit</button>
       </form>
-      <div>결과값이 들어올 곳</div>
+      <div>
+        {value > 0 && `수취금액은 ${exchange} ${select.slice(3)}입니다`}
+      </div>
     </section>
   );
 }
